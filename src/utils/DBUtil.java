7664ee1748861965ff89524de1dd5fee1ac75817
tech.s <techsame4@gmail.com>
2021-02-11 04:59:59 +0900

    Add User index

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME = "baseball_fictionary";
+    private static final String PERSISTENCE_UNIT_NAME = "BaseBallFictionary";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
