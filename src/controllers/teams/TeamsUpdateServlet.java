cacc155aa62586352429a19b79b33af050f8263d
tech.s <techsame4@gmail.com>
2021-03-01 22:39:32 +0900

    update teams create / update

--- src/controllers/teams/TeamsUpdateServlet.java
+++ src/controllers/teams/TeamsUpdateServlet.java
@@ -14,21 +14,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.League;
 import models.Team;
-import models.Title;
 import models.validators.TeamValidator;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class TeamsCreateServlet
+ * Servlet implementation class TeamsUpdateServlet
  */
-@WebServlet("/teams/create")
-public class TeamsCreateServlet extends HttpServlet {
+@WebServlet("/teams/update")
+public class TeamsUpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TeamsCreateServlet() {
+    public TeamsUpdateServlet() {
         super();
     }
 
@@ -40,49 +39,39 @@ public class TeamsCreateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            Title t = em.find(Title.class, Integer.parseInt(request.getParameter("id")));
-
-            Team te = new Team();
+            Team t = em.find(Team.class, (Integer)(request.getSession().getAttribute("team_id")));
 
             try {
                 League l = em.find(League.class, Integer.parseInt(request.getParameter("league_name")));
-                te.setLeagues(l);
+                t.setLeagues(l);
             } catch (Exception e) { }
 
-
-            request.getSession().setAttribute("title_id", t);
-
-            te.setTeam_name(request.getParameter("team_name"));
-
-            te.setTitles((Title)request.getSession().getAttribute("title_id"));
-            te.setTeam_information(request.getParameter("team_information"));
+            t.setTeam_name(request.getParameter("team_name"));
+            t.setTeam_information(request.getParameter("team_information"));
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            te.setCreated_at(currentTime);
-            te.setUpdated_at(currentTime);
+            t.setUpdated_at(currentTime);
 
-            List<String> errors = TeamValidator.validate(te);
+            List<String> errors = TeamValidator.validate(t);
             if(errors.size() > 0) {
                 em.close();
 
-                request.setAttribute("titles", t);
-                request.setAttribute("teams", te);
                 request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("teams", t);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/teams/new.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/teams/edit.jsp");
                 rd.forward(request, response);
             } else {
                 em.getTransaction().begin();
-                em.persist(te);
                 em.getTransaction().commit();
                 em.close();
-                request.getSession().setAttribute("flush", "登録が完了しました。");
+                request.getSession().setAttribute("flush", "更新が完了しました。");
 
-                response.sendRedirect(request.getContextPath() + "/teams/index?id=" + t.getTitle_id());
+                request.getSession().removeAttribute("team_id");
+
+                response.sendRedirect(request.getContextPath() + "/teams/index?id=" + t.getTitles().getTitle_id());
             }
-
-            request.getSession().removeAttribute("title_id");
         }
     }
 
