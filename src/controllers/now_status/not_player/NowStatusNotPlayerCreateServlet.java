50f94d7bd7fdc54458808119303323268eb84f1d
tech.s <techsame4@gmail.com>
2021-03-08 20:35:46 +0900

    Add state not create

--- src/controllers/now_status/not_player/NowStatusNotPlayerCreateServlet.java
+++ src/controllers/now_status/not_player/NowStatusNotPlayerCreateServlet.java
@@ -1,4 +1,4 @@
-package controllers.now_status.player;
+package controllers.now_status.not_player;
 
 import java.io.IOException;
 import java.sql.Timestamp;
@@ -13,24 +13,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Character_list;
+import models.NotPlayer;
 import models.NowStatus;
-import models.Player;
-import models.Team;
 import models.Title;
 import models.validators.NowStatusValidator;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class NowStatusCreateServlet
+ * Servlet implementation class NowStatusNotPlayerCreateServlet
  */
-@WebServlet("/status/player/create")
-public class NowStatusPlayerCreateServlet extends HttpServlet {
+@WebServlet("/status/not/create")
+public class NowStatusNotPlayerCreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NowStatusPlayerCreateServlet() {
+    public NowStatusNotPlayerCreateServlet() {
         super();
     }
 
@@ -45,10 +44,9 @@ public class NowStatusPlayerCreateServlet extends HttpServlet {
             Title titles = em.find(Title.class, Integer.parseInt(request.getParameter("id")));
 
             NowStatus n = new NowStatus();
-            Player p = new Player();
+            NotPlayer np = new NotPlayer();
 
             Character_list c = em.find(Character_list.class, Integer.parseInt(request.getParameter("chara_name")));
-            Team t = em.find(Team.class, Integer.parseInt(request.getParameter("team_name")));
 
             n.setCharacters(c);
 
@@ -56,52 +54,35 @@ public class NowStatusPlayerCreateServlet extends HttpServlet {
                 n.setNow_year(Integer.parseInt(request.getParameter("now_year")));
             } catch(Exception e){ }
 
-            n.setChara_flag(0);
+            //0.player 1.not_player
+            n.setChara_flag(1);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             n.setCreated_at(currentTime);
             n.setUpdated_at(currentTime);
 
-            //登録名を自動入力
-            if(request.getParameter("player_name") == null || request.getParameter("player_name").equals("")){
-                p.setPlayer_name(c.getChara_name());
+            //登録名と読み方が空欄の場合、characterの名前と読み方を入れる
+            if(request.getParameter("not_player_name") == null || request.getParameter("not_player_name").equals("")){
+                np.setNot_player_name(c.getChara_name());
             } else {
-                p.setPlayer_name(request.getParameter("player_name"));
+                np.setNot_player_name(request.getParameter("not_player_name"));
             }
-            if(request.getParameter("player_name_read") == null || request.getParameter("player_name_read").equals("")){
-                p.setPlayer_name_read(c.getChara_name_read());
+            if(request.getParameter("not_player_name_read") == null || request.getParameter("not_player_name_read").equals("")){
+                np.setNot_player_name_read(c.getChara_name_read());
             } else {
-                p.setPlayer_name_read(request.getParameter("player_name_read"));
+                np.setNot_player_name_read(request.getParameter("not_player_name_read"));
             }
 
-            p.setTeams(t);
+            np.setChara_type1(Integer.parseInt(request.getParameter("chara_type1")));
+            np.setChara_type2(Integer.parseInt(request.getParameter("chara_type2")));
+            np.setChara_type3(Integer.parseInt(request.getParameter("chara_type3")));
 
-            p.setPosision1(Integer.parseInt(request.getParameter("posision1")));
-            p.setPosision2(Integer.parseInt(request.getParameter("posision2")));
-            p.setPosision3(Integer.parseInt(request.getParameter("posision3")));
-            p.setPosision4(Integer.parseInt(request.getParameter("posision4")));
-            p.setPosision5(Integer.parseInt(request.getParameter("posision5")));
+            np.setChara_type_detail(request.getParameter("Chara_type_detail"));
 
-            p.setPosision_detail(request.getParameter("posision_detail"));
-            p.setNumber(request.getParameter("number"));
+            np.setNot_player_information(request.getParameter("not_player_information"));
 
-            p.setThrowing(Integer.parseInt(request.getParameter("throwing")));
-            p.setBatting(Integer.parseInt(request.getParameter("batting")));
-            p.setPlayer_type1(Integer.parseInt(request.getParameter("player_type1")));
-            p.setPlayer_type2(Integer.parseInt(request.getParameter("player_type2")));
-
-            try{
-                p.setSalary(Integer.parseInt(request.getParameter("salary")));
-            } catch(Exception e){ }
-
-
-            p.setMusic(request.getParameter("music"));
-            p.setPlayer_information(request.getParameter("player_information"));
-            p.setPerformance(request.getParameter("performance"));
-            p.setAther_player_information(request.getParameter("ather_player_information"));
-
-            p.setCreated_at(currentTime);
-            p.setUpdated_at(currentTime);
+            np.setCreated_at(currentTime);
+            np.setUpdated_at(currentTime);
 
             List<String> errors = NowStatusValidator.validate(n, c, true);
             if(errors.size() > 0) {
@@ -109,41 +90,35 @@ public class NowStatusPlayerCreateServlet extends HttpServlet {
                         .setParameter("titles", titles)
                         .getResultList();
 
-                List<Team> teams = em.createNamedQuery("getMyAllTeams", Team.class)
-                        .setParameter("titles", titles)
-                        .getResultList();
-
                 em.close();
 
                 request.setAttribute("now_status", n);
-                request.setAttribute("players", p);
+                request.setAttribute("not_players", np);
                 request.setAttribute("characters", characters);
                 request.setAttribute("titles", titles);
-                request.setAttribute("teams", teams);
                 request.setAttribute("_token", request.getSession().getId());
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/status/player/new.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/status/not/new.jsp");
                 rd.forward(request, response);
             } else {
                 em.getTransaction().begin();
                 em.persist(n);
                 em.getTransaction().commit();
 
-
-                p.setNow_status(n);
+                np.setNow_status(n);
 
                 em.getTransaction().begin();
-                em.persist(p);
+                em.persist(np);
                 em.getTransaction().commit();
 
-                n.setPlayers(p);
+                n.setNot_players(np);
 
                 em.getTransaction().begin();
                 em.persist(n);
                 em.getTransaction().commit();
 
-
+                //作中の現在年度の詳細情報の場合、自動で最新情報に入る
                 if(titles.getYear() + titles.getElapsed_year() == n.getNow_year()){
                     c.setNow_status(n);
 
